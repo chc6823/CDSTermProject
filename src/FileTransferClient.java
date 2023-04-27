a5b2a448711eda9a82bca85d18be01e2ba3b7d2f
chan Heo <chc6823@naver.com>
2023-04-28 02:33:31 +0900

    Version of Without Spring - 7

--- src/FileTransferClient.java
+++ src/FileTransferClient.java
@@ -11,48 +11,50 @@ public class FileTransferClient {
     public static void main(String[] args) throws IOException {
         // 클라이언트 스레드 풀 생성
         ExecutorService clientThreadPool = Executors.newFixedThreadPool(10);
-        // 서버에 전송할 파일 선택
-        JFileChooser fileChooser = new JFileChooser();
-        fileChooser.setMultiSelectionEnabled(true);
-        int result = fileChooser.showOpenDialog(null);
-        if (result != JFileChooser.APPROVE_OPTION) {
-            return;
-        }
-        File[] selectedFiles = fileChooser.getSelectedFiles();
-        for (File file : selectedFiles) {
-            System.out.println("File Name : "+file.getName());
-        }
 
-        // 선택한 파일들을 서버에 전송
-        for (File file : selectedFiles) {
-            // 파일 이름 전송
-            Socket socket = new Socket(SERVER_ADDRESS, PORT_NUMBER);
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            out.println(file.getName());
+        while (true) {
+            // 서버에 전송할 파일 선택
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setMultiSelectionEnabled(true);
+            int result = fileChooser.showOpenDialog(null);
+            if (result != JFileChooser.APPROVE_OPTION) {
+                break;
+            }
+            File[] selectedFiles = fileChooser.getSelectedFiles();
+            for (File file : selectedFiles) {
+                System.out.println("File Name : "+file.getName());
+            }
 
-            // 파일 내용 전송
-            try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file))) {
-                byte[] buffer = new byte[1024];
-                int bytesRead;
-                OutputStream os = socket.getOutputStream();
-                while ((bytesRead = bis.read(buffer)) != -1) {
-                    socket.getOutputStream().write(buffer, 0, bytesRead);
-                    System.out.println(bytesRead);
+            // 선택한 파일들을 서버에 전송
+            for (File file : selectedFiles) {
+                // 파일 이름 전송
+                Socket socket = new Socket(SERVER_ADDRESS, PORT_NUMBER);
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                out.println(file.getName());
+
+                // 파일 내용 전송
+                try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file))) {
+                    byte[] buffer = new byte[1024];
+                    int bytesRead;
+                    OutputStream os = socket.getOutputStream();
+                    while ((bytesRead = bis.read(buffer)) != -1) {
+                        os.write(buffer, 0, bytesRead);
+                        System.out.println(bytesRead);
+                    }
+                    os.flush();
+                    socket.shutdownOutput();
                 }
-                os.flush();
-                socket.shutdownOutput();
-            }
 
-            // 파일 전송 완료 메시지 수신
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            String response;
-            while ((response = in.readLine()) != null) {
-                System.out.println(response);
-            }
+                // 파일 전송 완료 메시지 수신
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String response;
+                while ((response = in.readLine()) != null) {
+                    System.out.println(response);
+                }
 
-            // 소켓 닫기
-            socket.close();
+                // 소켓 닫기
+                socket.close();
+            }
         }
     }
-
 }
--- src/FileTransferServer.java
+++ src/FileTransferServer.java
@@ -1,22 +1,34 @@
 import java.net.*;
 import java.io.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.concurrent.*;
 
 public class FileTransferServer {
     private static final int PORT_NUMBER = 4444;
+    private ArrayList<Socket> clients; // 접속한 클라이언트들의 소켓 정보를 저장하는 ArrayList
+
+    public FileTransferServer() {
+        clients = new ArrayList<Socket>();
+    }
 
     public static void main(String[] args) throws IOException {
+        FileTransferServer server = new FileTransferServer();
+        server.startServer();
+    }
+
+    public void startServer() throws IOException {
         // 서버 소켓 생성
         ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
         System.out.println("Server started on port " + PORT_NUMBER);
 
         // 클라이언트 스레드 풀 생성
-        ExecutorService clientThreadPool = Executors.newFixedThreadPool(10);
+        ExecutorService clientThreadPool = Executors.newFixedThreadPool(5);
 
         // 클라이언트 접속 대기
         while (true) {
             Socket clientSocket = serverSocket.accept();
+            clients.add(clientSocket); // 새로운 클라이언트 소켓 정보를 ArrayList에 추가
             System.out.println("New client connected: " + clientSocket.getInetAddress());
 
             // 새로운 클라이언트의 소켓을 이용하여 클라이언트 쓰레드 생성
@@ -24,13 +36,32 @@ public class FileTransferServer {
             clientThreadPool.execute(clientThread);
         }
     }
+
+    public void removeClient(Socket clientSocket) {
+        clients.remove(clientSocket); // 클라이언트 소켓 정보를 ArrayList에서 삭제
+        System.out.println("Client disconnected: " + clientSocket.getInetAddress());
+        broadcastMessage("Client " + clientSocket.getInetAddress() + " disconnected.");
+    }
+
+    public void broadcastMessage(String message) {
+        for (Socket client : clients) {
+            try {
+                PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+                out.println(message);
+            } catch (IOException e) {
+                System.out.println("Error broadcasting message to client " + client.getInetAddress() + ": " + e);
+            }
+        }
+    }
 }
 
 class ClientThread implements Runnable {
     private Socket clientSocket;
+    private FileTransferServer server;
 
     public ClientThread(Socket clientSocket) {
         this.clientSocket = clientSocket;
+        server = new FileTransferServer();
     }
 
     public void run() {
@@ -38,18 +69,17 @@ class ClientThread implements Runnable {
             // 클라이언트와의 입출력 스트림 생성
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-
             // 클라이언트가 전송한 파일을 서버에 저장
             String fileName = in.readLine();
 
             // 파일이 있는 디렉토리 경로를 지정
             File outputFile = new File("C:\\Users\\chc68\\OneDrive\\바탕 화면\\컴공\\5-1\\협동분산시스템"
-                     + fileName);
+                    + fileName);
             FileOutputStream fos = new FileOutputStream(outputFile);
 
             byte[] buffer2 = new byte[1024];
             int bytesRead2;
-            while ((bytesRead2= clientSocket.getInputStream().read(buffer2)) != -1) {
+            while ((bytesRead2 = clientSocket.getInputStream().read(buffer2)) != -1) {
                 fos.write(buffer2, 0, bytesRead2);
             }
             fos.close();
@@ -58,19 +88,19 @@ class ClientThread implements Runnable {
             out.println("File " + fileName + " sent successfully.");
 
             // 클라이언트가 파일 전송을 마칠 때까지 대기
-            while (clientSocket.getInputStream().read() != -1) {}
+            while (clientSocket.getInputStream().read() != -1) {
+            }
 
         } catch (IOException e) {
             System.out.println("Error handling client: " + e);
         } finally {
             try {
                 // 클라이언트 소켓 닫기
+                server.removeClient(clientSocket); // 클라이언트 소켓 정보를 ArrayList에서 삭제
                 clientSocket.close();
-                System.out.println("Client disconnected: " + clientSocket.getInetAddress());
             } catch (IOException e) {
                 System.out.println("Error closing client socket: " + e);
             }
         }
     }
 }
-
